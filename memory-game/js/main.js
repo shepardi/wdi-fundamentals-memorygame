6f6730a5994af9afc98c23fab0c78985f2d8f9af
Isaac Shepard <ishepard@ucsd.edu>
2020-04-20 17:30:48 -0700

    added card game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,6 +1,29 @@
-let cardOne = "king"
-let cardTwo = "king"
-let cardThree = "queen"
-let cardFour = "queen"
 
-console.log("User flipped " + cardOne);
+//array to store our cards
+let cards = ["queen", "queen", "king", "king"];
+
+//empty array
+let cardsInPlay = [];
+
+//varaibles for cards the user flipped
+let cardOne = cards[0];
+let cardTwo = cards[1];
+
+//add the cards to the cardsInPlay array
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+
+//Check if two cards have been played
+if( cardsInPlay.length ===2){
+
+	//check if 1st card in cardsInPlay array is equal to the second
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You have found a match!");
+	}
+	else{
+		alert("Sorry, try again");
+	}
+}
