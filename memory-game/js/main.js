342bedf9c2a0d9f5f3231e6a8515e5a609d7a132
Isaac Shepard <ishepard@ucsd.edu>
2020-04-20 19:34:18 -0700

    Added card objects to our main.jss file

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,6 +1,27 @@
 
-//array to store our cards
-let cards = ["queen", "queen", "king", "king"];
+//array to store our card objects
+let cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 
 //empty array
 let cardsInPlay = [];
@@ -8,19 +29,21 @@ let cardsInPlay = [];
 //function that checks for a match
 function checkForMatch(){
 	if(cardsInPlay[0] === cardsInPlay[1]){
-		console.log("You found a match!");
+		alert("You found a match!");
 	}else{
-		console.log("Sorry, try again");
+		alert("Sorry, try again");
 	}
 }
 
 //function to store the cards that were flipped
 function flipCard(cardsID){
 
-	console.log( "User flipped " + cards[cardsID]);
+	console.log( "User flipped " + cards[cardsID].rank);
+	console.log( "User flipped " + cards[cardsID].suit);
+	console.log( "User flipped " + cards[cardsID].cardImage);
 
 	//add the card flipped into the cardsInPlay array
-	cardsInPlay.push(cards[cardsID]);
+	cardsInPlay.push(cards[cardsID].rank);
 
 	//call check for match function
 	if(cardsInPlay.length ===2){
